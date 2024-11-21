5f56cdd7cc205153230c9d40e2334fb7442ca9f0
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-21 19:24:39 +0530

    Update start.py

--- AnonXMusic/cplugins/start.py
+++ AnonXMusic/cplugins/start.py
@@ -1,6 +1,5 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from nexichat import nexichat
 
 @Client.on_message(filters.command("start"))
 async def start_message(client: Client, message: Message):
