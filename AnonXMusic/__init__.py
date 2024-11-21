5fb0938469e75a28045664627c2b10f1dc163be0
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-21 21:53:35 +0530

    Update __init__.py

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -6,44 +6,6 @@ from AnonXMusic.core.userbot import Userbot
 from AnonXMusic.misc import dbb, heroku
 
 from .logging import LOGGER
-
-cloneownerdb = mongodb.clone_owners
-
-async def load_clone_owners():
-    async for entry in cloneownerdb.find():
-        bot_id = entry["bot_id"]
-        user_id = entry["user_id"]
-        
-
-async def save_clonebot_owner(bot_id, user_id):
-    await cloneownerdb.update_one(
-        {"bot_id": bot_id},
-        {"$set": {"user_id": user_id}},
-        upsert=True
-    )
-async def get_clone_owner(bot_id):
-    data = await cloneownerdb.find_one({"bot_id": bot_id})
-    if data:
-        return data["user_id"]
-    return None
-
-async def delete_clone_owner(bot_id):
-    await cloneownerdb.delete_one({"bot_id": bot_id})
-    
-
-async def save_idclonebot_owner(clone_id, user_id):
-    await cloneownerdb.update_one(
-        {"clone_id": clone_id},
-        {"$set": {"user_id": user_id}},
-        upsert=True
-    )
-
-async def get_idclone_owner(clone_id):
-    data = await cloneownerdb.find_one({"clone_id": clone_id})
-    if data:
-        return data["user_id"]
-    return None
-
   
 dirr()
 git()
