6a8d77c18feccb77c308f41309f931f6d24a83ee
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-21 17:32:41 +0530

    Update clonestats.py

--- AnonXMusic/utils/clonestats.py
+++ AnonXMusic/utils/clonestats.py
@@ -1,7 +1,22 @@
 from AnonXMusic.utils.core.mongo import mongodb
+from config import OWNER_ID
 
-cloneownerdb = mongodb.cloneownerdb
 clonebotdb = mongodb.clonebotdb
+
+cloneownerdb = mongodb.clone_owners
+
+async def get_clone_owner(bot_id):
+    data = await cloneownerdb.find_one({"bot_id": bot_id})
+    if data:
+        return data["user_id"]
+    return None
+    
+async def is_owner(bot_id, user_id):
+    owner_id = await get_clone_owner(bot_id)
+    if owner_id == user_id or user_id == OWNER_ID or user_id in SUDOERS:
+        return True
+    return False
+
 
 def get_bot_users_collection(bot_id):
     from nexichat import db as mongodb
