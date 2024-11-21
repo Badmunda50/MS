7d00e95c573bd9f31fd3a9a71a4702a3d2739b45
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-21 14:31:33 +0530

    Update autoleave.py

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1002056907061
+                                and i.chat.id != -1002356967761
                             ):
                                 if left == 20:
                                     continue
