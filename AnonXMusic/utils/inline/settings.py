5ed704f3bf72a6547803f84c0273496a3d79da63
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-21 13:21:24 +0530

    Update settings.py

--- AnonXMusic/utils/inline/settings.py
+++ AnonXMusic/utils/inline/settings.py
@@ -1,67 +1,52 @@
 from typing import Union
 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
-from AnonXMusic import app
+from pyrogram.types import InlineKeyboardButton
 
 
-def help_pannel(_, START: Union[bool, int] = None):
-    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")]
-    second = [
-        InlineKeyboardButton(
-            text=_["BACK_BUTTON"],
-            callback_data=f"settingsback_helper",
-        ),
+def setting_markup(_):
+    buttons = [
+        [
+            InlineKeyboardButton(text=_["ST_B_1"], callback_data="AU"),
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
+        ],
+        [
+            InlineKeyboardButton(text=_["ST_B_2"], callback_data="PM"),
+        ],
+        [
+            InlineKeyboardButton(text=_["ST_B_4"], callback_data="VM"),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+        ],
     ]
-    mark = second if START else first
-    upl = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_1"],
-                    callback_data="help_callback hb1",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_2"],
-                    callback_data="help_callback hb2",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_3"],
-                    callback_data="help_callback hb3",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb6",
-                ),
-            ],
-            mark,
-        ]
-    )
-    return upl
+    return buttons
 
 
-def help_back_markup(_):
-    upl = InlineKeyboardMarkup(
+def vote_mode_markup(_, current, mode: Union[bool, str] = None):
+    buttons = [
         [
-            [
-                InlineKeyboardButton(
-                    text=_["BACK_BUTTON"],
-                    callback_data=f"settings_back_helper",
-                ),
-            ]
-        ]
-    )
-    return upl
+            InlineKeyboardButton(text="ᴠᴏᴛɪɴɢ ᴍᴏᴅᴇ ➤", callback_data="VOTEANSWER"),
+            InlineKeyboardButton(
+                text=_["ST_B_5"] if mode == True else _["ST_B_6"],
+                callback_data="VOMODECHANGE",
+            ),
+        ],
+        [
+            InlineKeyboardButton(text="-2", callback_data="FERRARIUDTI M"),
+            InlineKeyboardButton(
+                text=f"ᴄᴜʀʀᴇɴᴛ ➣ {current}",
+                callback_data="ANSWERVOMODE",
+            ),
+            InlineKeyboardButton(text="+2", callback_data="FERRARIUDTI A"),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["BACK_BUTTON"],
+                callback_data="settings_helper",
+            ),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+        ],
+    ]
+    return buttons
+
 
 def auth_users_markup(_, status: Union[bool, str] = None):
     buttons = [
@@ -83,15 +68,43 @@ def auth_users_markup(_, status: Union[bool, str] = None):
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
-    return upl
-    
-def private_help_panel(_):
+    return buttons
+
+
+def playmode_users_markup(
+    _,
+    Direct: Union[bool, str] = None,
+    Group: Union[bool, str] = None,
+    Playtype: Union[bool, str] = None,
+):
     buttons = [
         [
+            InlineKeyboardButton(text=_["ST_B_10"], callback_data="SEARCHANSWER"),
             InlineKeyboardButton(
-                text=_["S_B_4"],
-                url=f"https://t.me/{app.username}?start=help",
+                text=_["ST_B_11"] if Direct == True else _["ST_B_12"],
+                callback_data="MODECHANGE",
             ),
+        ],
+        [
+            InlineKeyboardButton(text=_["ST_B_13"], callback_data="AUTHANSWER"),
+            InlineKeyboardButton(
+                text=_["ST_B_8"] if Group == True else _["ST_B_9"],
+                callback_data="CHANNELMODECHANGE",
+            ),
+        ],
+        [
+            InlineKeyboardButton(text=_["ST_B_14"], callback_data="PLAYTYPEANSWER"),
+            InlineKeyboardButton(
+                text=_["ST_B_8"] if Playtype == True else _["ST_B_9"],
+                callback_data="PLAYTYPECHANGE",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["BACK_BUTTON"],
+                callback_data="settings_helper",
+            ),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
     return buttons
