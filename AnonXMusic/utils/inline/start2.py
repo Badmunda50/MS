b194be5ce72bb97d3edce9038747b1cf98e4dcd0
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-01-21 23:22:38 +0530

    Update start2.py

--- AnonXMusic/utils/inline/start2.py
+++ AnonXMusic/utils/inline/start2.py
@@ -1,31 +1,30 @@
 from pyrogram.types import InlineKeyboardButton
-
-import config
 from pyrogram import filters, Client
 
+import config
 
-def start_panel(_):
+def start_panel(client):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{client.me.username}?startgroup=true"
+                text=_["S_B_1"], url=f"https://t.me/{client.get_me().username}?startgroup=true"
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
 
-
-def private_panel(_):
+def private_panel(client):
+    user_id = client.get_me().id
     buttons = [
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{client.me.username}?startgroup=true",
+                url=f"https://t.me/{client.get_me().username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=user_id),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
