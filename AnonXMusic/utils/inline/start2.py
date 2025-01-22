3a19a29a7100b60efb3a2fcff22d6081252a7879
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-01-22 09:10:58 +0530

    Update start2.py

--- AnonXMusic/utils/inline/start2.py
+++ AnonXMusic/utils/inline/start2.py
@@ -1,20 +1,34 @@
 from pyrogram.types import InlineKeyboardButton
-from pyrogram import filters, Client
-
-import config
 from AnonXMusic import app
+import requests
+
+def get_cloner_username():
+    # Fetch the username of the person who cloned the repository
+    response = requests.get("https://api.github.com/repos/Badmunda50/MS")
+    if response.status_code == 200:
+        repo_data = response.json()
+        owner_username = repo_data["owner"]["login"]
+        return owner_username
+    return "Unknown"
 
 def start_panel(_):
+    cloner_username = get_cloner_username()
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_[ "S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_[ "S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"], url=f"https://t.me/{cloner_username}"
+            ),
         ],
     ]
     return buttons
-
+    
+    
 def private_panel(_, user_id):
     buttons = [
         [
@@ -24,7 +38,7 @@ def private_panel(_, user_id):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=user_id),
+            InlineKeyboardButton(text=_["S_B_5"], url=f"https://t.me/{cloner_username}"
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
